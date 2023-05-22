6679a2e52e3d146323a4a3fb2c1d804a06dc9922
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2023-05-22 19:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
-// function threeSum(arr, target) {
-// // write your code here
+
+
+function decimalToBinary(decimalNum) {
+  let binaryNum = "";
   
-// }
-
-// module.exports = threeSum;
-
-
-function letterCombinations(digits) {
-  if (digits.length === 0) {
-    return []; // Return an empty array for an empty digit string
+  if (decimalNum === 0) {
+    return "0";
   }
-
-  const digitToLetters = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz',
-  };
-
-  const combinations = ['']; // Start with an empty combination
-
-  for (const digit of digits) {
-    const letters = digitToLetters[digit];
-    const newCombinations = [];
-
-    for (const combination of combinations) {
-      for (const letter of letters) {
-        newCombinations.push(combination + letter);
-      }
-    }
-
-    combinations.length = 0; // Clear the existing combinations
-    combinations.push(...newCombinations); // Add new combinations
+  
+  while (decimalNum > 0) {
+    binaryNum = (decimalNum % 2) + binaryNum;
+    decimalNum = Math.floor(decimalNum / 2);
   }
-
-  return combinations.sort(); // Sort the combinations lexicographically
+  
+  return binaryNum;
 }
 
 // Example usage
-const digits = '23';
-const result = letterCombinations(digits);
-console.log(result);
+console.log(decimalToBinary(7));    // Output: 111
+console.log(decimalToBinary(10));   // Output: 1010
+console.log(decimalToBinary(33));   // Output: 100001
+
+
+
+// // function threeSum(arr, target) 
+// // // write your code here
+  
+// // }
+
+// // module.exports = threeSum;
+
+
+// function letterCombinations(digits) {
+//   if (digits.length === 0) {
+//     return []; // Return an empty array for an empty digit string
+//   }
+
+//   const digitToLetters = {
+//     '2': 'abc',
+//     '3': 'def',
+//     '4': 'ghi',
+//     '5': 'jkl',
+//     '6': 'mno',
+//     '7': 'pqrs',
+//     '8': 'tuv',
+//     '9': 'wxyz',
+//   };
+
+//   const combinations = ['']; // Start with an empty combination
+
+//   for (const digit of digits) {
+//     const letters = digitToLetters[digit];
+//     const newCombinations = [];
+
+//     for (const combination of combinations) {
+//       for (const letter of letters) {
+//         newCombinations.push(combination + letter);
+//       }
+//     }
+
+//     combinations.length = 0; // Clear the existing combinations
+//     combinations.push(...newCombinations); // Add new combinations
+//   }
+
+//   return combinations.sort(); // Sort the combinations lexicographically
+// }
+
+// // Example usage
+// const digits = '23';
+// const result = letterCombinations(digits);
+// console.log(result);
 
